c0baf455ca1deaa4911acd43f830e9a233828fc7
Mustafa <mustafaolmezses@icloud.com>
2024-09-04 14:35:57 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Mustafa Ölmezses on 4.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
