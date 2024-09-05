ebffe9908c5cb447bafb698165c2d0f55ed13f61
Mustafa <mustafaolmezses@icloud.com>
2024-09-05 22:47:46 +0300

    [FEATURE] add a RegisterView page design

--- TodoApp/Views/RegisterView.swift
+++ TodoApp/Views/RegisterView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var name: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text("Register View")
+        HeaderView()
+            .padding(.bottom , 60)
+        VStack{
+            TextField("Enter a name :", text: $name)
+                .modifier(TextFieldModifier())
+            TextField("Enter a E-mail adress :", text: $email)
+                .modifier(TextFieldModifier())
+            SecureFieldWithButton(text: $password, title: "Password")
+                .padding(.bottom)
+            
+            BigStrokeButton(title: "Sign Up") {
+                
+            }
+            
+        }
+        .padding(.bottom , 100)
     }
 }
+
 
 #Preview {
     RegisterView()
 }
+
+
