4c5a041695509cb0fe4005bb586f638737f9b3f0
Mustafa <mustafaolmezses@icloud.com>
2024-09-05 18:24:54 +0300

    [Feature] LoginView designed and Extracted

--- TodoApp/Views/LoginView.swift
+++ TodoApp/Views/LoginView.swift
@@ -8,11 +8,135 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                Spacer()
+                //HEADER
+                HeaderView()
+                //FORM
+                VStack{
+                    LoginFormsView()
+                    LoginGoogleView()
+                }
+                //FOOTER
+                Spacer()
+                Divider()
+                FooterView()
+                
+            }
+        }
     }
 }
 
 #Preview {
     LoginView()
+}
+
+struct HeaderView: View {
+    var body: some View {
+        ZStack{
+            Image("TodoImage")
+                .resizable()
+                .frame(width: 240 , height: 100)
+        }
+        .padding()
+    }
+}
+
+struct LoginFormsView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        VStack{
+            TextField("E-mail adress :", text: $email)
+                .textInputAutocapitalization(.never)
+                .font(.subheadline)
+                .padding(16)
+                .background(Color(.systemGray5))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.horizontal , 24)
+                .padding(.vertical , 8)
+            
+            SecureField("Password :", text: $password)
+                .textInputAutocapitalization(.never)
+                .font(.subheadline)
+                .padding(16)
+                .background(Color(.systemGray5))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(.horizontal , 24)
+            
+            Button("Forget password ? ") {
+                
+            }
+            .foregroundStyle(Color("ACText"))
+            .font(.footnote)
+            .bold()
+            .frame(maxWidth: .infinity , alignment: .trailing)
+            .padding(.horizontal , 20)
+            .padding(.vertical , 8)
+            .foregroundStyle(.black)
+            
+            
+            Button(action: {}, label: {
+                Text("Sign in")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity)
+            })
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(lineWidth: 2)
+                    .frame(height: 48)
+                    .padding(.horizontal , 4)
+            )
+            .padding()
+            
+        }
+    }
+}
+
+struct LoginGoogleView: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Rectangle()
+                    .frame(height: 1)
+                Text("Or")
+                Rectangle()
+                    .frame(height: 1)
+            }
+            .foregroundStyle(.gray)
+            .padding()
+            .padding()
+            
+            HStack{
+                Image("Google")
+                    .resizable()
+                    .frame(width: 60 , height: 60)
+                Button(action: {}, label: {
+                    Text("Sign in with your google account")
+                })
+            }
+            
+        }
+    }
+}
+
+struct FooterView: View {
+    var body: some View {
+        HStack{
+            Text("Don't your have an account ? ")
+            NavigationLink("Sign Up") {
+                RegisterView()
+            }
+            .bold()
+            .foregroundStyle(Color("ACText"))
+        }
+    }
 }
